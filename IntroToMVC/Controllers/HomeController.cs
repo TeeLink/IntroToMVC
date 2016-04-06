db5a1ca1614933bc7df01162aa406ae69ca2a44b
Tee Link <tesuanseylink@gmail.com>
2016-04-06 13:12:47 -0500

    touch-ups/completed

--- IntroToMVC/Controllers/HomeController.cs
+++ IntroToMVC/Controllers/HomeController.cs
@@ -32,22 +32,15 @@ namespace IntroToMVC.Controllers
         {
             List<Classmate> classmates = new List<Classmate>();
             {
-                //new Classmate() {Name = "Seth", HairColor = "Black", Height = 5.10};
-                //new Classmate() {Name = "Zach", HairColor = "Blonde", Height = 5.9};
-                //new Classmate() {Name = "Brian", HairColor = "Black", Height = 5.7};
-                //new Classmate() {Name = "Kate", HairColor = "Brown", Height = 5.6};
-                //new Classmate() {Name = "Kevin", HairColor = "Black", Height = 5.7};
-                //new Classmate() {Name = "Trey", HairColor = "Black", Height = 5.6};
-                new Classmate("Seth", "Black", 5.10);
-                new Classmate("Zach", "Blonde", 5.9);
-                new Classmate("Brian", "Black", 5.7);
-                new Classmate("Kate", "Brown", 5.6);
-                new Classmate("Kevin", "Black", 5.7);
-                new Classmate("Trey", "Black", 5.6);
+                classmates.Add(new Classmate() { Name = "Seth Tettah-Quaye", HairColor = "Black", Height = 5.10 });
+                classmates.Add (new Classmate() { Name = "Zach Ballard", HairColor = "Blonde", Height = 5.9 });
+                classmates.Add(new Classmate() { Name = "Brian Stickney", HairColor = "Black", Height = 5.7 });
+                classmates.Add(new Classmate() { Name = "Kate Ramsey", HairColor = "Brown", Height = 5.6 });
+                classmates.Add(new Classmate() { Name = "Kevin Mach", HairColor = "Black", Height = 5.7 });
+                classmates.Add(new Classmate() { Name = "Trey Holmes", HairColor = "Black", Height = 5.6 });
             }
-            ;
-
-            return View();
+            
+            return View(classmates);
 
         }
     }
