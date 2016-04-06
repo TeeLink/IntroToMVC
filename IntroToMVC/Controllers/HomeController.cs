63f387b95a1d3c0e280266b0f5a007733d49eeb4
Tee Link <tesuanseylink@gmail.com>
2016-04-06 01:03:32 -0500

    list attempt

---
+++ IntroToMVC/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IntroToMVC.Models;
+
+namespace IntroToMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Classmates()
+        {
+            List<Classmate> classmates = new List<Classmate>();
+            {
+                new Classmate("Seth", "Black", 5.10);
+                new Classmate("Zach", "Blonde", 5.9);
+                new Classmate("Brian", "Black", 5.7);
+                new Classmate("Kate", "Brown", 5.6);
+                new Classmate("Kevin", "Black", 5.7);
+                new Classmate("Trey", "Black", 5.6);
+            }
+            return View();
+
+        }
+    }
+}
---
+++ IntroToMVC/Models/Classmates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IntroToMVC.Controllers;
+
+namespace IntroToMVC.Models
+{
+    public class Classmate : HomeController
+    {
+        public Classmate(string name, string hairColor, double height)
+        {
+            Name = name;
+            HairColor = hairColor;
+            Height = height;
+        }
+        //public int Id { get; set; }
+        public string Name { get; set; }
+        public string HairColor { get; set; }
+        public double Height { get; set; }
+    }
+}
